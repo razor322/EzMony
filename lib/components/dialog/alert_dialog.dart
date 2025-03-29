8318c5a3f97cbb33081cd85d97d463d5eb6443de
gybran nauval <nauvalgybran@gmail.com>
2025-03-30 01:52:23 +0700

    feat : add new constants style, componet and extension

---
+++ lib/components/dialog/alert_dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AlertDialog extends StatelessWidget {
+  const AlertDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
